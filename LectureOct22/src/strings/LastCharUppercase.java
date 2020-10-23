a64e47ba159d6b29f39e7cc97ab622401769de77
Gabriel <fendtg4@gmail.com>
2020-10-22 20:36:32 -0400

    finished lastCharUppercase

--- LectureOct22/src/strings/LastCharUppercase.java
+++ LectureOct22/src/strings/LastCharUppercase.java
@@ -3,7 +3,14 @@ package strings;
 public class LastCharUppercase {
 
 	public static void main(String[] args) {
-		
-		
+		String s="hello hi good morning I am enjoying programming with java";
+		StringBuilder sb=new StringBuilder();
+		String ar[]=s.split(" ");
+		for(String s1:ar) {
+			sb.append(s1.substring(0,s1.length()-1)).append(Character.toString(Character.toUpperCase(s1.charAt(s1.length()-1)))).append(" ");
+		}
+		System.out.println(sb.toString().trim());
 	}
+
 }
+//Task - Convert every words last char to uppercase
